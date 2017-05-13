5b47705b63c5c6d08eb77d9092adbb8765404088
Compu Trabajo <nrtyr2016@gmail.com>
2017-05-13 15:21:43 -0500

    proyeccion

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Reporte de Proyecciones</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<script type="text/javascript" src="bootstrap.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+		  <div class="col-md-6 col-md-offset-3 panel panel-success">
+		  	<h1>Reporte de Proyecciones</h1>
+		  	<br>
+
+
+				<form action="insertar.php" method="post">
+					<div class="form-group">
+
+					    <label>Tipo de evento:</label>
+
+					    <select name="optEvento" class="form-control">
+					    	<option value="PROYECCIÓN">PROYECCIÓN</option>
+					    </select>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Nombre del Anfitrión:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Comunidad :</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Calle:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>C.P.:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Sección:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Fecha:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Hora Convocada:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Hora Término:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Responsable de la proyección:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Imagenes:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Imagenes:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+
+					<div class="form-group">
+
+					    <label>Observaciones:</label>
+
+					    <input type="text" name="txtComunidad" placeholder="Comunidad" id="autoSeccion" autocomplete="on" class="form-control"/>
+					</div>
+				</form>
+
+
+
+
+		  </div>
+		</div>
+	</div>
+</body>
+</html>
